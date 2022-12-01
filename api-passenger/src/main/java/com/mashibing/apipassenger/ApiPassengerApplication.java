dbb391e4c26959a413dcd4caa80912fc489efc63
mutkeb <1182878813@qq.com>
2022-12-01 23:38:43 +0800

    first

---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/ApiPassengerApplication.java
@@ -0,0 +1,14 @@
+package com.mashibing.apipassenger;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassengerApplication {
+
+
+    public static void main(String[] args){
+        SpringApplication.run(ApiPassengerApplication.class);
+    }
+}
---
+++ api-passenger/src/main/java/com/mashibing/apipassenger/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.mashibing.apipassenger.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test(){
+        return "test api passenger";
+    }
+}
