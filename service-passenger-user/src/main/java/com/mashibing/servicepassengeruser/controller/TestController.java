7d2a56c61c0cf2f4761bb3cedd4b3f4ea79ac661
mutkeb <1182878813@qq.com>
2022-12-04 23:51:52 +0800

    创建乘客用户服务

---
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.mashibing.servicepassengeruser.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    public String test(){
+        return "service-passenger-user";
+    }
+}
