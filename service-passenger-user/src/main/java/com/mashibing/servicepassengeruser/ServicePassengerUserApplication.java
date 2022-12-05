d6a33a3d904dbcf5b6e4ee1f50c04ac36ebd783e
mutkeb <1182878813@qq.com>
2022-12-05 14:12:22 +0800

    注册到nacos

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/ServicePassengerUserApplication.java
@@ -4,9 +4,12 @@ package com.mashibing.servicepassengeruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.mashibing.servicepassengeruser.mapper")
+@EnableDiscoveryClient
+
 public class ServicePassengerUserApplication {
 
     public static void main(String[] args) {
