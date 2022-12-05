36f365b752979f204632816128fba77e0c07b7d7
mutkeb <1182878813@qq.com>
2022-12-05 13:40:56 +0800

    插入用户记录

--- service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/mashibing/servicepassengeruser/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,10 +28,24 @@ public class UserService {
         Map<String,Object> map = new HashMap<>();
         map.put("passenger_phone",passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers == null ? "无记录": passengerUsers.get(0).getPassengerPhone());
-        //  判断用户信息是否存在
+        System.out.println(passengerUsers.size() == 0 ? "无记录": passengerUsers.get(0).getPassengerPhone());
 
-        //  如果不存在，插入用户信息
+        //  判断用户信息是否存在
+        if(passengerUsers.size() == 0){
+            //  如果不存在，插入用户信息
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte) 0);
+
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+
+        }
 
         return ResponseResult.success();
     }
