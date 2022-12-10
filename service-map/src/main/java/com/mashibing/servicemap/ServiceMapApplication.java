551ad1214c77cddcacb704adcb3353530677fac4
mutkeb <1182878813@qq.com>
2022-12-10 14:17:48 +0800

    地图调用规划

--- service-map/src/main/java/com/mashibing/servicemap/ServiceMapApplication.java
+++ service-map/src/main/java/com/mashibing/servicemap/ServiceMapApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -11,5 +13,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class ServiceMapApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceMapApplication.class);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
--- service-map/src/main/java/com/mashibing/servicemap/remote/MapDirectionClient.java
+++ service-map/src/main/java/com/mashibing/servicemap/remote/MapDirectionClient.java
@@ -4,8 +4,11 @@ package com.mashibing.servicemap.remote;
 import com.mashibing.internalcommon.constant.AmapConfigConstant;
 import com.mashibing.internalcommon.response.DirectionResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
@@ -13,6 +16,9 @@ public class MapDirectionClient {
 
     @Value("${amap.key}")
     private String amapKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     public DirectionResponse direction(String depLongitude, String depLatitude, String destLongitude, String destLatitude){
         //  组装参数调用url
@@ -31,9 +37,10 @@ public class MapDirectionClient {
         urlBuild.append("key=" + amapKey);
         log.info(urlBuild.toString());
         //  调用高德接口
-
+        ResponseEntity<String> directionEntity = restTemplate.getForEntity(urlBuild.toString(), String.class);
+        log.info("高德地图路径规划，返回的信息：" + directionEntity.getBody());
         //  解析接口
-
+        
 
 
         return null;
