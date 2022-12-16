274514ae67973326f567c488606e68ac1c1ac515
mutkeb <1182878813@qq.com>
2022-12-16 11:17:29 +0800

    司机车辆绑定代码完善

--- internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/mashibing/internalcommon/constant/CommonStatusEnum.java
@@ -31,6 +31,19 @@ public enum CommonStatusEnum {
     MAP_DISTRICT_ERROR(1400,"地图政区错误"),
 
     /**
+     * 司机和车辆 1500~1599
+     */
+    DRIVER_CAR_BIND_NOT_EXISTS(1500,"司机和车辆绑定关系不存在"),
+
+    DRIVER_NOT_EXISTS(1501,"司机不存在"),
+
+    DRIVER_CAR_BIND_EXISTS(1502,"司机和车辆绑定关系已存在,请勿重复绑定"),
+
+    DRIVER_BIND_EXISTS(1503,"司机已被绑定,请勿重复绑定"),
+
+    CAR_BIND_EXISTS(1504,"车辆已被绑定,请勿重复绑定"),
+
+    /**
      * 成功
      */
     SUCCESS(1,"success"),
--- service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverCarBindingRelationshipService.java
+++ service-driver-user/src/main/java/com/mashibing/servicedriveruser/service/DriverCarBindingRelationshipService.java
@@ -1,5 +1,7 @@
 package com.mashibing.servicedriveruser.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mashibing.internalcommon.constant.CommonStatusEnum;
 import com.mashibing.internalcommon.constant.DriverCarConstant;
 import com.mashibing.internalcommon.dto.DriverCarBindingRelationship;
 import com.mashibing.internalcommon.dto.ResponseResult;
@@ -24,6 +26,33 @@ public class DriverCarBindingRelationshipService {
     private DriverCarBindingRelationshipMapper driverCarBindingRelationshipMapper;
 
     public ResponseResult bind(DriverCarBindingRelationship driverCarBindingRelationship){
+        QueryWrapper<DriverCarBindingRelationship> queryWrapper = new QueryWrapper();
+        queryWrapper.eq("driver_id",driverCarBindingRelationship.getDriverId());
+        queryWrapper.eq("car_id",driverCarBindingRelationship.getCarId());
+        queryWrapper.eq("bind_state",1);
+
+        Integer integer = driverCarBindingRelationshipMapper.selectCount(queryWrapper);
+        if (integer.intValue() > 0){
+            return ResponseResult.fail(CommonStatusEnum.DRIVER_CAR_BIND_EXISTS.getCode(),CommonStatusEnum.DRIVER_CAR_BIND_EXISTS.getValue());
+        }
+
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("car_id",driverCarBindingRelationship.getCarId());
+        queryWrapper.eq("bind_state",1);
+        integer = driverCarBindingRelationshipMapper.selectCount(queryWrapper);
+        if (integer.intValue() > 0){
+            return ResponseResult.fail(CommonStatusEnum.CAR_BIND_EXISTS.getCode(),CommonStatusEnum.CAR_BIND_EXISTS.getValue());
+        }
+
+        queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("driver_id",driverCarBindingRelationship.getDriverId());
+        queryWrapper.eq("bind_state",1);
+        integer = driverCarBindingRelationshipMapper.selectCount(queryWrapper);
+        if (integer.intValue() > 0){
+            return ResponseResult.fail(CommonStatusEnum.DRIVER_BIND_EXISTS.getCode(),CommonStatusEnum.DRIVER_BIND_EXISTS.getValue());
+        }
+
+
         LocalDateTime now = LocalDateTime.now();
         driverCarBindingRelationship.setBindingTime(now);
         driverCarBindingRelationship.setBindState(DriverCarConstant.DRIVER_CAR_BIND);
