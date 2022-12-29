53498e1c6bcce18471e42d5f7c5e288815f7c53b
mutkeb <1182878813@qq.com>
2022-12-29 19:23:51 +0800

    司机客户端模拟

---
+++ ssh-driver-client-web/src/main/java/sshdriverclientweb/SshDriverClientWebApplication.java
@@ -0,0 +1,12 @@
+package sshdriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshDriverClientWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SshDriverClientWebApplication.class);
+    }
+}
